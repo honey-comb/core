cdaaa19acc16f9b0fcb5f4483d49dd9299a7c441
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-01 19:41:46 +0200

    Updated list with checkbox

--- src/resources/assets/react/hc-admin-list/List.js
+++ src/resources/assets/react/hc-admin-list/List.js
@@ -10,7 +10,9 @@ export default class List extends Component {
         this.state = {
             records: {
                 data: []
-            }
+            },
+            listId: uuid(),
+            headers: {}
         };
 
         this.getDataRowField = this.getDataRowField.bind(this);
@@ -21,20 +23,22 @@ export default class List extends Component {
             .then(res => {
 
                 const data = res.data;
-
-                this.setState({records: data});
+                this.setState({
+                    records: data
+                });
             });
     }
 
     render() {
-        let id = uuid();
-
         return <div id="list">
-            <table id={id} className="table table-bordered table-striped dataTable" role="grid">
+            <table id={this.state.listId} className="table table-hover table-bordered dataTable" role="grid">
                 <thead>
                 <tr role="row">
+                    <th hidden={this.props.hideCheckBox} className="main-checkbox"><input type="checkbox"/></th>
                     {Object.keys(this.props.headers).map((item, i) => (
-                            <th tabIndex="0" aria-controls={id} key={i}
+                            <th tabIndex="0"
+                                aria-controls={this.state.listId}
+                                key={i}
                                 className="sorting">{this.props.headers[item].label}</th>
                         )
                     )}
@@ -54,7 +58,7 @@ export default class List extends Component {
     getDataRow(record, key) {
 
         return <tr id={record.id} key={key}>
-
+            <td hidden={this.props.hideCheckBox}><input type="checkbox"/></td>
             {Object.keys(this.props.headers).map((item, i) => (
                     this.getDataRowField(item, record[item], i)
             ))}
@@ -64,7 +68,8 @@ export default class List extends Component {
     getDataRowField(id, value, key)
     {
         if (id === 'id')
-            return <td key={key} hidden={true}>{value}</td>;
+            return <td key={key}
+                       hidden={true}>{value}</td>;
 
         switch (this.props.headers[id].type)
         {
