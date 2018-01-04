6906ca00b5ba3abd117cf11b951b2cd2bbceb3c0
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-04 10:55:59 +0200

    Added disable property

--- src/resources/assets/react/hc-form/buttons/FAButton.js
+++ src/resources/assets/react/hc-form/buttons/FAButton.js
@@ -7,7 +7,10 @@ export default class FAButton extends Component {
 
     render() {
 
-        let classes = classNames (this.props.type, {"hidden": this.props.display});
+        let classes = classNames (this.props.type, {
+            "hidden": this.props.display,
+            "disabled": this.props.disabled
+        });
 
         return <div className={classes} onMouseUp={this.props.onPress}>
             <FontAwesomeIcon icon={this.props.icon}/>
