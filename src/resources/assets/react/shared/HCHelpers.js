af446ae17ba3fceca1fb90a9d4c0a701e72a890e
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-04 20:55:54 +0200

    Added helper function to compare arrays

--- src/resources/assets/react/shared/HCHelpers.js
+++ src/resources/assets/react/shared/HCHelpers.js
@@ -2,20 +2,39 @@ window.HCHelpers = {
 
     faPrefix: "far",
 
-    faIcon: function (name, prefix)
-    {
+    faIcon: function (name, prefix) {
         if (!prefix)
             prefix = this.faPrefix;
 
         return [prefix, name]
     },
 
-    buttonClass: function (name)
-    {
+    buttonClass: function (name) {
         if (!name)
             name = "clean";
 
         return "btn btn-" + name;
+    },
+
+    arraysEqual: function (arr1, arr2) {
+
+        if (Object.size(arr1) !== Object.size(arr2))
+            return false;
+
+        let keys = Object.keys(arr1);
+
+        for(let i = keys.length; i--;)
+        {
+            if (arr1[keys[i]] !== arr2[keys[i]])
+            {
+                if (typeof (arr1[keys[i]]) === "object" && typeof (arr2[keys[i]]) === "object")
+                    return HCHelpers.arraysEqual(arr1[keys[i]], arr2[keys[i]]);
+
+                return false;
+            }
+        }
+
+        return true;
     }
 };
 
