2f70a5a26695c7e1374b33b028aac770f6d3de15
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-07 15:53:24 +0200

    Added countrer hiding logic

--- src/resources/assets/react/hc-form/buttons/FAButton.js
+++ src/resources/assets/react/hc-form/buttons/FAButton.js
@@ -7,7 +7,7 @@ export default class FAButton extends Component {
 
     render() {
 
-        let divClasses = classNames (this.props.type, {
+        let divClasses = classNames(this.props.type, {
             "hidden": this.props.display,
             "disabled": this.props.disabled
         });
@@ -20,13 +20,16 @@ export default class FAButton extends Component {
         if (this.props.count === 0)
             hideCounter = true;
 
-        let counterClasses = classNames (
-          "counter", "fa-layers-counter", {
-              "hidden": hideCounter
+        let counterClasses = classNames(
+            "counter", "fa-layers-counter", {
+                "hidden": hideCounter
             }
         );
 
-        return <div className={divClasses} onMouseUp={this.props.onPress}>
+        return <div className={divClasses} onMouseUp={(e) => {
+            if (!this.props.disabled && this.props.onPress)
+                this.props.onPress()
+        }}>
             <FontAwesomeIcon icon={this.props.icon}/>
             <span className={counterClasses}>{this.props.count}</span>
         </div>;
