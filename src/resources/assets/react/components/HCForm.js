d057abe9d87382a69cf80771f1a53b95935f136f
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-13 20:50:45 +0200

    Improved form template

--- src/resources/assets/react/components/HCForm.js
+++ src/resources/assets/react/components/HCForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import TweenMax from "gsap"
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import axios from "axios/index";
 
 export default class HCForm extends Component {
 
@@ -12,40 +13,63 @@ export default class HCForm extends Component {
         };
 
         this.state = {
-            id: HC.helpers.uuid()
+            id: HC.helpers.uuid(),
+            formData: {}
         };
 
         this.opacity = 0;
     }
 
     render() {
-        return <div ref="formHolder" id={this.state.id} className="hc-form" style={{opacity:this.opacity}}>
+        return <div ref="formHolder" id={this.state.id} className="hc-form" style={{opacity: this.opacity}}>
             <div className="header">
-                <div className="close" style={{float:"left"}} onClick={() => this.animateForm(false)}>
+                <div className="close" style={{float: "left"}} onClick={() => this.animateForm(false)}>
                     <FontAwesomeIcon icon={HC.helpers.faIcon('times-circle')}/>
                 </div>
                 <div className="label">{this.props.contentID ? "Edit record" : "New record"}</div>
             </div>
+            <div className="form-structure">
+                {this.getFields()}
+            </div>
+            <div className="footer">
+                {this.getButtons()}
+            </div>
         </div>;
     }
 
-    componentDidMount ()
-    {
-        this.animateForm (true);
+    componentDidMount() {
+        this.animateForm(true);
+        this.loadFormData();
     }
 
-    animateForm (forward)
-    {
-        if (forward)
-        {
+    /**
+     * Loading form data
+     */
+    loadFormData() {
+        let url = this.props.config.url;
+
+        axios.get(url)
+            .then(res => {
+
+                this.setState({
+                    formData: res.data,
+                });
+            });
+    }
+
+    /**
+     * Animate form
+     *
+     * @param forward
+     */
+    animateForm(forward) {
+        if (forward) {
             TweenMax.to(this, 0.5, {
                 opacity: 1,
                 onUpdate: () => this.refs.formHolder.style.opacity = this.opacity,
             });
         }
-        else
-        {
-            console.log('backwards');
+        else {
 
             TweenMax.to(this, 0.5, {
                 opacity: 0,
@@ -54,4 +78,36 @@ export default class HCForm extends Component {
             });
         }
     }
+
+    getFields ()
+    {
+        console.log(this.state.formData.structure);
+    }
+
+    getButtons ()
+    {
+        let buttons = this.state.formData.buttons;
+        let finalButtons = [];
+
+        if (!buttons) {
+            return "";
+        }
+
+        Object.keys(buttons).map((item, i) => (
+            finalButtons.push(this.getButton (item, buttons[item], i))
+        ));
+
+        return finalButtons;
+    }
+
+    getButton (type, data, i)
+    {
+        switch (type)
+        {
+            case "submit" :
+
+                return <div key={i} className={HC.helpers.buttonClass('primary')}>{data.label}</div>;
+        }
+    }
+
 }
