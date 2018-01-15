6f85804fa8f34d59fb7e50a520d622cac2de3d1b
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-16 00:28:27 +0200

    Updated dashboard

--- src/resources/views/admin/dashboard.blade.php
+++ src/resources/views/admin/dashboard.blade.php
@@ -2,37 +2,34 @@
 
 @section('content')
     <div class="row">
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
+        <div class="info-box">
                 <span class="info-box-icon bg-yellow">
                     {!! fontAwesomeIcon('users') !!}
                 </span>
 
-                <div class="info-box-content">
-                    <span class="info-box-text">{{ trans('HCCore::core.users') }}</span>
-                    <span class="info-box-number">{{ \HoneyComb\Core\Models\HCUser::count() }}</span>
-                </div>
+            <div class="info-box-content">
+                <span class="info-box-text">{{ trans('HCCore::core.users') }}</span>
+                <span class="info-box-number">{{ \HoneyComb\Core\Models\HCUser::count() }}</span>
             </div>
-            <div class="info-box">
+        </div>
+        <div class="info-box">
                 <span class="info-box-icon bg-green">
                     {!! fontAwesomeIcon('hdd') !!}
                 </span>
 
-                <div class="info-box-content">
-                    <span class="info-box-text">{{ trans('HCCore::core.size') }}</span>
-                    <span class="info-box-number">{{ getProjectFileSize() }}</span>
-                </div>
+            <div class="info-box-content">
+                <span class="info-box-text">{{ trans('HCCore::core.size') }}</span>
+                <span class="info-box-number">{{ getProjectFileSize() }}</span>
             </div>
-            <div class="info-box">
+        </div>
+        <div class="info-box">
                 <span class="info-box-icon bg-light-blue">
                     {!! fontAwesomeIcon('database') !!}
                 </span>
 
-                <div class="info-box-content">
-                    <span class="info-box-text">{{ trans('HCCore::core.size') }}</span>
-                    <span class="info-box-number">{{ getProjectDbSize() }}</span>
-                </div>
+            <div class="info-box-content">
+                <span class="info-box-text">{{ trans('HCCore::core.size') }}</span>
+                <span class="info-box-number">{{ getProjectDbSize() }}</span>
             </div>
         </div>
     </div>
