4b470d7d25bcc804f29c640d828277a1662459b6
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-19 22:21:09 +0200

    refactored drop down list

---
+++ src/resources/assets/react/hc-form/fields/DropDownList.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Base from "./BaseField";
+
+export default class DropDownList extends Base {
+
+    getInput ()
+    {
+        return <select className="form-control"
+                       ref="inputField"
+                       disabled={this.props.config.disabled || this.props.config.readonly}
+                       onChange={this.contentChange}>
+
+            {this.getOptions()}
+
+        </select>
+    }
+
+    /**
+     * Creating select options
+     *
+     * @returns {Array}
+     */
+    getOptions ()
+    {
+        let list = [];
+
+        if (!this.props.config.required)
+        {
+            list.push (<option key={-1} value="undefined">Please select:</option>)
+        }
+
+        this.props.config.options.map((item, i) => list.push(<option key={i} value={item.id}>{item.label}</option>));
+
+        return list;
+    }
+
+    /**
+     * If input required validate first option
+     */
+    componentDidMount()
+    {
+        if(this.props.config.required)
+            this.validate();
+    }
+
+    /**
+     * Validating input
+     *
+     * @returns {boolean}
+     */
+    isValid ()
+    {
+        if (this.props.config.required)
+            if (this.props.config.options.indexOf(this.refs.inputField.value) === -1)
+                return false;
+
+        return true;
+    }
+
+    /**
+     * Getting value
+     * 
+     * @returns {undefined}
+     */
+    getValue ()
+    {
+        if (this.refs.inputField.value === "undefined")
+            return undefined;
+
+        return this.refs.inputField.value;
+    }
+
+}
