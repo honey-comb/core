503eb1a1e0297e60f61a9f35ee1a45b121c7819d
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-28 14:04:34 +0200

    Added Media field

--- src/resources/assets/react/components/HCForm.js
+++ src/resources/assets/react/components/HCForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import TweenMax from "gsap"
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import axios from "axios/index";
 import Email from "../hc-form/fields/Email";
 import Password from "../hc-form/fields/Password";
@@ -8,6 +8,7 @@ import CheckBoxList from "../hc-form/fields/CheckBoxList";
 import BaseField from "../hc-form/fields/BaseField";
 import DropDownList from "../hc-form/fields/DropDownList";
 import TextArea from "../hc-form/fields/TextArea";
+import Media from "../hc-form/fields/Media";
 
 export default class HCForm extends Component {
 
@@ -202,6 +203,10 @@ export default class HCForm extends Component {
             case "textArea" :
 
                 return <TextArea key={i} config={data} ref={ref} id={ref}/>;
+
+            case "media" :
+
+                return <Media key={i} config={data} ref={ref} id={ref}/>;
         }
 
         return "";
---
+++ src/resources/assets/react/hc-form/fields/Media.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import Base from "./BaseField";
+import Thumbnail from "./media/Thumbnail";
+
+export default class Media extends Base {
+
+    constructor(props) {
+        super(props);
+
+        this.count = this.props.config.count ? this.props.config.count : 1;
+        this.multiple = !(this.count === 1);
+        this.thumbnails = [];
+        this.state = {
+            count: 0
+        };
+
+        this.mediaList = [];
+
+        this.fileListChange = this.fileListChange.bind(this);
+        this.thumbnailUpdated = this.thumbnailUpdated.bind(this);
+    }
+
+    /**
+     *
+     * @returns {*[]}
+     */
+    getInput() {
+        return <div className="thumbnails">{this.generateThumbnails()}</div>
+    }
+
+    /**
+     *
+     * @param e
+     */
+    fileListChange(e) {
+
+        let files = this.refs.inputField.files;
+
+        Object.keys(files).map((item, i) => this.createThumbnailFromFile(files[item], i));
+    }
+
+    /**
+     * Creating thumbnail from file
+     * @param file
+     * @param i
+     */
+    createThumbnailFromFile(file, i) {
+        if (file.size > this.props.config.size * 1024) {
+            console.log('File to big');
+            return;
+        }
+
+        this.createThumbnail(file, null, i);
+    }
+
+    /**
+     * Creating media from existing id
+     * @param media
+     * @param i
+     */
+    createThumbnailFromMedia (media, i)
+    {
+        this.createThumbnail(null, media, i);
+    }
+
+    /**
+     * Creating thumbnail
+     *
+     * @param file
+     * @param mediaId
+     * @param i
+     */
+    createThumbnail (file, mediaId, i)
+    {
+        this.state.count++;
+
+        this.thumbnails.push(<Thumbnail file={file}
+                                        mediaId={mediaId}
+                                        key={i}
+                                        uploadUrl={this.props.config.uploadUrl}
+                                        viewUrl={this.props.config.viewUrl}
+                                        onChange={this.thumbnailUpdated}/>);
+
+        this.setState({count: this.state.count});
+    }
+
+    /**
+     * Generating all of the thumbnails
+     *
+     * @returns {*[]}
+     */
+    generateThumbnails() {
+
+        //TODO if data is available
+
+        return [...this.thumbnails, this.getAddButton()];
+    }
+
+    /**
+     * Creating add more button
+     *
+     * @returns {*}
+     */
+    getAddButton() {
+        if (this.count <= this.state.count) {
+            return "";
+        }
+
+        return <div className="hc-media-uploader">
+            <label>
+                <input type="file"
+                       ref="inputField"
+                       multiple={this.multiple}
+                       accept={this.props.config.accept}
+                       onChange={this.fileListChange}/>
+                <span>Upload media (max: {this.count - this.state.count})</span>
+            </label>
+        </div>;
+    }
+
+    /**
+     * Thumbnail has been updated
+     *
+     * @param config
+     */
+    thumbnailUpdated(config)
+    {
+        switch (config.action)
+        {
+            case "uploaded":
+
+                this.mediaList.push(config.id);
+                break;
+
+            case "removed":
+
+                this.mediaList.push(config.id);
+                break;
+        }
+
+        this.contentChange();
+    }
+
+    /**
+     * Setting value
+     * @param data
+     */
+    setValue (data)
+    {
+        if(this.count === 1)
+            if (!Array.isArray(data))
+                this.createThumbnailFromMedia(data, 0);
+
+        //TODO: create from multiple images
+    }
+
+    /**
+     * Getting value
+     */
+    getValue() {
+
+        if(this.count === 1)
+            if (this.mediaList[0])
+                return this.mediaList[0];
+
+
+        return this.mediaList;
+    }
+}
