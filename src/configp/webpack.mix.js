7975eb19c79f2d5e772208f3ab9e0a2a3fdad16a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-04-16 09:21:31 +0300

    updated default react config

--- src/configp/webpack.mix.js
+++ src/configp/webpack.mix.js
@@ -1,13 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.copy('node_modules/font-awesome/fonts/fontawesome-webfont.woff2', 'public/fonts/fontawesome-webfont.woff2');
+mix.sass("resources/assets/honey-comb/sass/honey-comb.scss", "public/css/hc-admin-panel.css");
 
-mix.sass(
-    "resources/assets/honey-comb/sass/honey-comb.scss",
+mix.react('resources/assets/honey-comb/components/full.js', 'public/js/hc-full.js');
 
-    "public/css/hc-admin-panel.css").version();
-
-mix.react('resources/assets/honey-comb/components/full.js', 'public/js/hc-full.js').version();
-mix.react('resources/assets/honey-comb/components/font-awesome.js', 'public/js/font-awesome.js').version();
-
-mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js').version();
+mix.version();
