fbbb0ea711a10408a6a61c043fc74d420d697aed
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-05-19 11:52:34 +0300

    Added new AdminList Cell field DateTime

---
+++ src/resources/assets/react/hc-form/fields/HCCellDateTime.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+export default class HCCellDateTime extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.getValue = this.getValue.bind(this);
+    }
+
+    render() {
+        return <div>
+            {this.getValue()}
+        </div>;
+    }
+
+    getValue() {
+        let time = new Date(this.props.value);
+        time.addHours(this.props.config.utc);
+
+        return time.getFullYear() + '-' + time.getFullMonth() + '-' + time.getFullDay() + ' ' +
+            time.getFullHours() + ':' + time.getFullMinutes() + ':' + time.getFullSeconds()
+    }
+}
