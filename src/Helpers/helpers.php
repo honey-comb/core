39140ac48f4f22ddc6a8fa63ef6a1b83f3b4baa1
Darius <superbow4@gmail.com>
2018-06-07 18:21:50 +0300

    add helper to add in array before

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -307,7 +307,7 @@ if (!function_exists('optimizeTranslationOptions')) {
      */
     function optimizeTranslationOptions(Collection $list)
     {
-        return $list->map(function($record) {
+        return $list->map(function ($record) {
 
             return optimizeSingleTranslationOption($record);
         });
@@ -430,3 +430,77 @@ if (!function_exists('array_splice_after_key')) {
         return $array;
     }
 }
+
+/*
+* @Source: http://eosrei.net/comment/287
+*
+* Inserts a new key/value before the key in the array.
+*
+* @param $key
+*   The key to insert before.
+* @param $array
+*   An array to insert in to.
+* @param $new_key
+*   The key to insert.
+* @param $new_value
+*   An value to insert.
+*
+* @return
+*   The new array if the key exists, FALSE otherwise.
+*
+* @see array_insert_after()
+*/
+if (!function_exists('array_insert_before')) {
+    function array_insert_before($key, array &$array, $new_key, $new_value)
+    {
+        if (array_key_exists($key, $array)) {
+            $new = [];
+            foreach ($array as $k => $value) {
+                if ($k === $key) {
+                    $new[$new_key] = $new_value;
+                }
+                $new[$k] = $value;
+            }
+
+            $array = $new;
+        }
+    }
+}
+
+/*
+ * @Source: http://eosrei.net/comment/287
+ *
+ * Inserts a new key/value after the key in the array.
+ *
+ * @param $key
+ *   The key to insert after.
+ * @param $array
+ *   An array to insert in to.
+ * @param $new_key
+ *   The key to insert.
+ * @param $new_value
+ *   An value to insert.
+ *
+ * @return
+ *   The new array if the key exists, FALSE otherwise.
+ *
+ * @see array_insert_before()
+ */
+if (!function_exists('array_insert_after')) {
+    function array_insert_after($key, array &$array, $new_key, $new_value)
+    {
+        if (array_key_exists($key, $array)) {
+
+            $new = [];
+            foreach ($array as $k => $value) {
+                $new[$k] = $value;
+                if ($k == $key) {
+                    $new[$new_key] = $new_value;
+                }
+            }
+
+            $array = $new;
+        }
+    }
+}
+
