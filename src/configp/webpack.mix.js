f01ff200b91f1a1671bb9d2b197a7fb798b747ff
Mantas Paulauskas <mantopastas@gmail.com>
2018-06-27 10:50:10 +0300

    possible fix with npm run dev command /admin-lte/dist/img/boxed-bg.jpg issue

--- src/configp/webpack.mix.js
+++ src/configp/webpack.mix.js
@@ -1,7 +1,9 @@
 let mix = require('laravel-mix');
 
-mix.sass("resources/assets/honey-comb/sass/honey-comb.scss", "public/css/hc-admin-panel.css");
-
+mix.sass("resources/assets/honey-comb/sass/honey-comb.scss", "public/css/hc-admin-panel.css").options({
+    processCssUrls: false
+});
+sdfsdfsdfdsf
 mix.react('resources/assets/honey-comb/components/full.js', 'public/js/hc-full.js');
 
 mix.version();
