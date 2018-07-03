596c1e54e8f89179ecc9337c983e26e6760a42f8
Mantas Paulauskas <mantopastas@gmail.com>
2018-07-03 18:07:46 +0300

    package update

--- src/configp/webpack.mix.js
+++ src/configp/webpack.mix.js
@@ -3,7 +3,7 @@ let mix = require('laravel-mix');
 mix.sass("resources/assets/honey-comb/sass/honey-comb.scss", "public/css/hc-admin-panel.css").options({
     processCssUrls: false
 });
-sdfsdfsdfdsf
+
 mix.react('resources/assets/honey-comb/components/full.js', 'public/js/hc-full.js');
 
 mix.version();
