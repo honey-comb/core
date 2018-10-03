9b1025467e749223dbeb01a435c7d40afa1452ef
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-10-03 16:11:19 +0300

    Email validation fix

--- src/resources/assets/react/form/fields/Email.js
+++ src/resources/assets/react/form/fields/Email.js
@@ -2,8 +2,13 @@ import React from 'react'
 import BaseField from "./BaseField";
 
 export default class Email extends BaseField {
-    isValid ()
-    {
+    isValid() {
+        let email = this.getValue();
+
+        if (email.length === 0 && !this.props.config.required) {
+            return true;
+        }
+
         return HC.helpers.validateEmail(this.getValue());
     }
 }
